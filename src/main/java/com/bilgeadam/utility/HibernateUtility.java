98fd87e244821cd3115a8c0ccb0e04d1283de5fe
musty1406 <musty1406@gmail.com>
2023-07-19 10:30:30 +0300

    Initial commit

---
+++ src/main/java/com/bilgeadam/utility/HibernateUtility.java
@@ -0,0 +1,31 @@
+package com.bilgeadam.utility;
+
+import com.bilgeadam.repository.entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtility {
+
+    private static final SessionFactory SESSION_FACTORY=createSessionFactory();
+
+    private static SessionFactory createSessionFactory() {
+
+        try {
+            Configuration configuration=new Configuration();
+            configuration.addAnnotatedClass(Users.class);
+            configuration.addAnnotatedClass(Book.class);
+            configuration.addAnnotatedClass(Author.class);
+            configuration.addAnnotatedClass(Address.class);
+            configuration.addAnnotatedClass(UserInformation.class);
+            configuration.addAnnotatedClass(Borrow.class);
+            SessionFactory sessionFactory=configuration.configure("hibernate.cfg.xml").buildSessionFactory();
+           return  sessionFactory;
+        }catch (Exception e){
+            e.printStackTrace();
+            return  null;
+        }
+
+
+    }
+
+}
